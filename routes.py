71e43115a27d34ce6facf0880072ce79fb831e05
kathyslate <mercyoyetunde98@gmail.com>
2024-10-21 18:40:03 +0100

    sbout us

--- routes.py
+++ routes.py
@@ -46,6 +46,14 @@ def login():
     
     return render_template('login.html', form=form)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/logout')
 @login_required
 def logout():
